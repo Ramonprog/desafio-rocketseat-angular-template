25ed2e6337d0c1b71bf1c075534a2f4112fe91c8
Ramonprog <programador.ramon@gmail.com>
2025-10-06 14:44:42 -0300

    feat: adicionando formGroup aos inputs de filtro

--- frontend-gestao-marketplace/src/app/pages/products/products.ts
+++ frontend-gestao-marketplace/src/app/pages/products/products.ts
@@ -3,17 +3,24 @@ import { ProductsService } from '../../services/products';
 import { take } from 'rxjs';
 import { IProduct } from '../../interfaces/IProduct';
 import { IProductResponseList } from '../../interfaces/product-response-list';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
 export class Products {
   private readonly _productsService = inject(ProductsService);
   products: IProductResponseList[] = [];
+
+  filterForm = new FormGroup({
+    title: new FormControl(''),
+    status: new FormControl(''),
+  });
+
   //ngOnInit lifecycle que chama a func assim que o componente for iniciado
   ngOnInit() {
     this._productsService
